9ec7d0f4f3b5597d61cc91a47e5139073b0053be
pavani <gorlagup1@hawkmail.newpaltz.edu>
2020-03-12 23:54:40 -0400

    changes made

---
+++ client/src/components/HelloWorld.vue
@@ -0,0 +1,97 @@
+<template>
+<nav class="navbar" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+   
+    
+
+    <a :class="{ 'is-active':isOpen }" @click="isOpen = !isOpen" role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" >
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+    </a>
+  </div>
+
+  <div :class="{ 'is-active':isOpen }" class="navbar-menu">
+    <div class="navbar-start">
+      
+
+
+      <router-link to="/Home" class="navbar-item" active-class="is-current" exact>Home</router-link>|
+      <router-link to="/About" class="navbar-item" active-class="is-current" exact>About</router-link>|
+      
+      
+
+      
+
+      <router-link to="/Excercise" class="navbar-item" active-class="is-current">Excercise</router-link>|
+      
+
+      
+      <router-link to="/Sign-upt" class="navbar-item" active-class="is-current">Sign-up</router-link>|
+      <router-link to="/Login" class="navbar-item" active-class="is-current">Login</router-link>
+      
+      <div class="navbar-item has-dropdown is-hoverable">
+         
+         <div class="navbar-dropdown">
+          <router-link to="/Exercise" class="navbar-item">
+            Exercise</router-link>
+          </div>
+      </div>
+          
+<div class="navbar-item has-dropdown is-hoverable">
+        <a class="navbar-item">
+          About</a>
+        
+
+        <div class="navbar-dropdown">
+          <router-link to="/contact" class="navbar-item">
+            Contact</router-link>
+          
+           <hr class="navbar-divider">
+          <router-link to="/report" class="navbar-item">
+            Report an issue</router-link>
+          
+         
+          
+        </div>
+      </div>
+    </div>  
+  
+
+    <div class="navbar-end">
+      <div class="navbar-item">
+        
+          <router-link to="/sign" class="navbar-item">
+            Sign up</router-link>
+        
+          <router-link to="/login" class="navbar-item">
+            Log in</router-link>
+          
+        
+      </div>
+    </div>
+  </div>
+</nav>
+</template>
+
+<script>
+export default {
+  data: ()=>({
+    isOpen: false     
+  })
+}
+</script>
+
+<style>
+  .is-current {
+    font-weight: bold;
+    color: blue;
+    text-align: right;
+  }
+  .navbar {
+    font: 1em sans-serif;
+    text-align: center;
+    font-weight: 500;
+    color: #000;
+  }
+</style>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,60 +1,10 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+var express = require('express');
+var router = express.Router();
 
-import AboutUs from '../views/AboutUs.vue'
-import Community from '../views/Community.vue'
-
-import Track from '../views/Track.vue'
-import Activity from '../views/Activity.vue'
-import Log from '../views/Log.vue'
-import Share from '../views/Share.vue'
-
-import UserSupport from '../views/UserSupport.vue'
-import Call from '../views/Call.vue'
-import Chat from '../views/Chat.vue'
-import Email from '../views/Email.vue'
-
-import Login from '../views/Login.vue'
-import SignUp from '../views/SignUp.vue'
-import { CurrentUser } from '../models/Users';
-
-Vue.use(VueRouter)
-
-const routes = [
-  { path: '/', name: 'Home', component: Home },
-  { path: '/AboutUs', name: 'AboutUs', component: AboutUs },
-  { path: '/Community', name: 'Community', component: Community },
-  { path: '/Track', name: 'Track', component: Track},
-  { path: '/Activity', name: 'Activity', component: Activity, meta: { isSecret: true } },
-  { path: '/Log', name: 'Log', component: Log },
-  { path: '/Share', name: 'Share', component: Share },
-  { path: '/UserSupport', name: 'UserSupport', component: UserSupport },
-  { path: '/Call', name: 'Call', component: Call },
-  { path: '/Chat', name: 'Chat', component: Chat },
-  { path: '/Email', name: 'Email', component: Email },
-  { path: '/Login', name: 'Login', component: Login },
-  { path: '/SignUp', name: 'SignUp', component: SignUp },
-
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
-
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
-})
-
-router.beforeEach((to, from, next) => {
-  if(to.meta.isSecret && !CurrentUser) next('/Login');
-  else next();
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
 });
 
-export default router
+module.exports = router;
+  
