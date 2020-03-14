d9ca073b88219a8248a27f2432d7e6b16e85e265
pavani <gorlagup1@hawkmail.newpaltz.edu>
2020-03-13 20:55:32 -0400

    --- Auto-Git Commit ---

--- client/src/views/Exercise.vue
+++ client/src/views/Exercise.vue
@@ -3,7 +3,7 @@
   
     <div class="container">
       <div class="title">
-      <img src="./assets/exercise.jpg" width="250" height="190">
+      <img src="../assets/exercise.jpg" width="250" height="190">
      <h2 class="has-text-black has-text-weight-bold has-text-right">
           Walking 
         
