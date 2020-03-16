52bb78b88323a2db2a81ff57a46058f44648d34d
pavani <gorlagup1@hawkmail.newpaltz.edu>
2020-03-15 21:59:14 -0400

    --- Auto-Git Commit ---

--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -1,77 +1,66 @@
 <template>
   <nav class="navbar is-link" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
-        <router-link exact-active-class="active" class="navbar-item" to="/">
+        <a class="navbar-item router-link-active" href="/">
             <i class="fas fa-home fa-pull-left"></i>
             Home
-        </router-link>
+            </a>
+       
 
-        <a @click.prevent="isOpen = !isOpen" role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+        <a class="navbar-burger burger" role="button" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
             <span aria-hidden="true"></span>
             <span aria-hidden="true"></span>
             <span aria-hidden="true"></span>
         </a>
     </div>
 
-    <div id="navbarBasicExample" class="navbar-menu" :class="{ 'is-active' : isOpen }">
+    <div id="navbarBasicExample" class="navbar-menu" >
         <div class="navbar-start">
-
-        <router-link exact-active-class="active" class="navbar-item" to="/Login">
+<a class="navbar-item" href="/Login">
             <i class="fab fa-vuejs fa-pull-left"></i>
             Login
-        </router-link>
+        </a>
 
       <div class="navbar-item has-dropdown is-hoverable">
-      <router-link exact-active-class="active" class="navbar-item" to="/About">
+        <a class="navbar-item" href="/About">
+     
             <i class="fab fa-vuejs fa-pull-left"></i>
             About
-        </router-link>
-     
-       <router-link exact-active-class="active" class="navbar-item" to="/Sign-Up">
-            <i class="fab fa-vuejs fa-pull-left"></i>
-          Sign-Up
-        </router-link>
-      </div>
-    </div>
-
-  <div class="navbar-item has-dropdown is-hoverable">
-  
-  <a class="navbar-link">
-      Exercise
-    </a>
-
+            </a>
+       <div class="navbar-item has-dropdown is-hoverable">
+         <a class="navbar-link">
+           Exercise
+           </a>
     <div class="navbar-dropdown">
-       <router-link exact-active-class="active" class="navbar-item" to="/Contact Me">
-            <i class="fab fa-vuejs fa-pull-left"></i>
-        Contact
-      </router-link>
-      <hr class="navbar-divider">
-       <router-link exact-active-class="active" class="navbar-item" to="/report">
-            <i class="fab fa-vuejs fa-pull-left"></i>
-        Report an issue
-      </router-link>
-    </div>
-  </div>
-</div>
-
-    <div class="navbar-end">
-      <div class="navbar-item">
-        <div class="buttons">
-        <router-link exact-active-class="active" class="navbar-item" to="/newuser">
-          <a class="button is-primary">
-            <strong>New User</strong>
-          </a>
-        </router-link>
-        <router-link exact-active-class="active" class="navbar-item" to="/login">
-          <a class="button is-primary">
-            <strong>Log in</strong>
-          </a>
-        </router-link>
-        </div>
+      <a class="navbar-item" href="/Contact Me">
+      <i class="fab fa-vuejs fa-pull-left"></i>
+      Contact
+      </a>
+    <hr class="navbar-divider">
+    <a class="navbar-item" href="/Sign-Up">
+    <i class="fab fa-vuejs fa-pull-left">
+      </i>
+      Sign-Up
+      </a>
       </div>
-    </div>
-  
-</nav>
+      </div>
+      </div>
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <div class="buttons">
+            <a class="navbar-item" href="/newuser">
+            <a class="button is-primary">
+              <strong> New User </strong>
+              </a>
+              </a>
+              </div>
+              </div>
+              </div>
+              </nav>
+              <!---->
+              </div>
+              <!--built files will be auto injected-->
+
 </template>
 <script>
 export default {
