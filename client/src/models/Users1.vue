7e3c8fdee0135d46b0cfc56575ce5907e7900d9e
pavani <gorlagup1@hawkmail.newpaltz.edu>
2020-03-17 00:37:30 -0400

    --- Auto-Git Commit ---

--- client/src/models/Users1.vue
+++ client/src/models/Users1.vue
@@ -1,17 +1,16 @@
-const Users = [
-    
+const Users1 = [
     { Name: 'Pavani', Password: '2020', Email: 'gorlagup1@hawkmail.newpaltz.edu' },
     { Name: 'Pavan', Password: '5780', Email: '547gpavani@newpaltz.edu' },
     
 ];
 
-export let CurrentUsers1 = null;
+export let CurrentUser = null;
 
-export function SignUp(Username,email, password) {
+export function Sign-UP(email, password) {
 
-    const users1 = Users1.find(x => x.Email == email);
-    if(!users1) throw Error('User not found');
-    if(user1.users1.Password != password) throw Error('Wrong Password');   
+    const user = Users.find(x => x.Email == email);
+    if(!user1) throw Error('User not found');
+    if(user.Password != password) throw Error('Wrong Password');   
 
-    return CurrentUsers1 = users1;
+    return CurrentUser1 = user1;
 }
