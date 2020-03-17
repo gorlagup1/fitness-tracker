9cdeeda0deec324142e42dc04f79d5c5c672eeae
pavani <gorlagup1@hawkmail.newpaltz.edu>
2020-03-17 18:42:27 -0400

    --- Auto-Git Commit ---

--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -1,58 +1,66 @@
 <template>
-<nav class="navbar" role="navigation" aria-label="main navigation">
-  <div class="navbar-brand">
-    <a class="navbar-item">
-       <img src="../assets/exercise.jpg" width="250" height="190" display:none>
-    </a>
+<nav class="navbar is-dark" role="navigation" aria-label="main navigation">
 
-    <a :class="{ 'is-active':isOpen }" @click="isOpen = !isOpen" role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" >
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-    </a>
-  </div>
 
+<div class="navbar-brand">
+  <figure class="image is-128x128">
+    <img class="is-rounded" src="../assets/logo1.png">
+    </figure>
+</div>
   <div :class="{ 'is-active':isOpen }" class="navbar-menu">
+
     <div class="navbar-start">
 
-
-      <router-link to="/" class="navbar-item" active-class="is-current" exact>Home</router-link>
-      <router-link to="/about" class="navbar-item" active-class="is-current">About</router-link>
-      <router-link to="/exercise" class="navbar-item" active-class="is-current">Exercise</router-link>
-      <router-link to="/Signin" class="navbar-item" active-class="is-current">Signin</router-link>
-
-
+      <router-link to="/" class="navbar-item is-large has-text-weight-bold" active-class="is-current" exact>Home</router-link>
       <div class="navbar-item has-dropdown is-hoverable">
-        <a class="navbar-link">
-          More Options
-        </a>
-
+      <div class="navbar-item has-text-weight-bold" >About</div>
+      
         <div class="navbar-dropdown">
-          <router-link to="/Contact Me" class="navbar-item" active-class="is-current" exact>Contact Me</router-link>
-          <router-link to="/Login" class="navbar-item" active-class="is-current" exact>Login</router-link>
-          <router-link to="/Sign-Up" class="navbar-item" active-class="is-current" exact>Sign-Up</router-link>
-         
+          <router-link to="/AboutUs" class="navbar-item has-text-weight-bold">About</router-link>
+          <hr class="navbar-divider">
+          <router-link to="/Community" class="navbar-item has-text-weight-bold">Community</router-link>
+        </div>
+      </div>     
+
+<div class="navbar-item has-dropdown is-hoverable">
+      <div class="navbar-item has-text-weight-bold" active-class="is-current">Sign-Up</div>
+      
+        <div class="navbar-dropdown">
+          <router-link to="/Activity" class="navbar-item has-text-weight-bold">Exercise </router-link>
+          <hr class="navbar-divider">
+          <router-link to="/Log" class="navbar-item has-text-weight-bold">Login</router-link>
+          <hr class="navbar-divider">
+          <router-link to="/Share" class="navbar-item has-text-weight-bold">More</router-link>
         </div>
       </div>
-    </div>
 
-    <div class="navbar-end">
+  <div class="navbar-item has-dropdown is-hoverable">
+      <div class="navbar-item has-text-weight-bold" active-class="is-current">Exercise</div>
+      
+        <div class="navbar-dropdown">
+          <router-link to="/Call" class="navbar-item has-text-weight-bold">Contact Me </router-link>
+          <hr class="navbar-divider">
+          <router-link to="/Chat" class="navbar-item has-text-weight-bold">Login</router-link>
+          <hr class="navbar-divider">
+          <router-link to="/Email" class="navbar-item has-text-weight-bold">About</router-link>
+        </div>
+      </div>
+      </div>   
+
+         <div class="navbar-end">
       <div class="navbar-item">
         <div class="buttons">
-          <a class="button is-primary" href="/Sign-Up">
-            <strong>Sign-up</strong>
-          </a>
-          <a class="button is-light" href="/Login">
-           <strong> Login </strong>
-          </a>
-          <a class="button is-primary" href="/Signin">
-          <strong>Signin</strong>
-          </a>
+          <router-link to="/SignUp" class="button is-primary has-text-weight-bold">Sign up</router-link>
+          <router-link to="/Login" class="button is-light has-text-weight-bold">Login</router-link>
         </div>
       </div>
     </div>
-  </div>
+</div> 
+
 </nav>
+
+
+
 </template>
 
 <script>
@@ -64,8 +72,7 @@ export default {
 </script>
 
 <style>
-  .is-current {
-    font-weight: bold;
-    color: blueviolet
+navbar {
+    color: #08b6ad;
   }
 </style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Fitness App Logo" src="../assets/logo1.png" height="120" width="100">
+    <img alt="Fitness App Logo" src="../assets/logo1.png" height="1200" width="140">
     <h1 class="title is-1">Welcome to Simple Fitness-Tracker App</h1>
     <HelloWorld msg="Balenced mind in the  balenced body?"/>
   </div>
@@ -8,7 +8,7 @@
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 export default {
   name: 'Home',
   components: {
