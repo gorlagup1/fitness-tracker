c345ebaed393847ef054b9031a5b0383f8846bc2
pavani <gorlagup1@hawkmail.newpaltz.edu>
2020-05-05 17:01:07 -0400

    --- Auto-Git Commit ---

---
+++ server/index.js
@@ -0,0 +1,41 @@
+const express = require = require('express');
+const path = require('path');
+const aboutController = require('./controller/about');
+const exerciseController = require('./controller/exercise');
+const foodListController = require('./controller/foodList');
+const loginController = require('./controller/login');
+const weightController = require('./controller/users');
+const meetingBoardController = require('./controller/meetingBoard');
+const usersController = require('./controller/users');
+
+const app = express();
+const port = 400;
+app.use(function(req,res, next){
+    res.header("Access-Control-Allow-Origin","*");
+    res.header("Acess-Control-Allow-Headers","Origin, x-Requeted-With, Content-Type, Accept, Authorization");
+    next();
+});
+app.user(function(req, res, next){
+    const arr = (req.headers/.authorization || "").split(" ");
+    if(arr.length > 1 && arr[1] !=null){
+        req.userId = +arr[1];
+    }
+    next();
+});
+app.use(express.json())
+.use(express.urlencoded({extended: true }))
+.use('/about', aboutController)
+.use('/exercise', exerciseController)
+.use('/users', usersController)
+.use('/meetingboard', meetingboardController)
+.use('/weight', weightController)
+.use('/login', loginController)
+.use('/food', foodListController)
+.use('/about', aboutController)
+.use((err,req,res,next)=>{
+    console.error(err);
+    const errorCode = err.code || 500;
+    res.status(errorCode).send({message:err.message });
+})
+app.listen(port, () => console.log('Listening at http://localhost:$(port}'));
+
---
+++ server/models/Users.js
@@ -0,0 +1,12 @@
+const User = [
+    {Name : 'John Michel',username:'JohnMichel547', Email:'JohnMichel547@23.com', Password:'JohnMichel', isAdmin:false, userID:0},
+    {Name : 'Easy', Username: 'scooby', Email:'scooby@76.com', Password:'scooby', isAdmin:true, userID:1}
+];
+module.exports={
+    Login(username, password){
+        const user = User.find(x=>x.Username == username);
+        if(!user) throw Error('User not found');
+        if(user.Password != password) throw Error('wrong Password');
+        return user;
+    }
+}
