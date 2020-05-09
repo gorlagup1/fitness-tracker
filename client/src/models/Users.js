71f9285a86ea0c0635560c68659f844015e7265f
pavani <gorlagup1@hawkmail.newpaltz.edu>
2020-05-09 19:12:27 -0400

    --- Auto-Git Commit ---

--- client/src/models/Users.js
+++ client/src/models/Users.js
@@ -2,10 +2,8 @@ import myFetch from "./myFetch";
 
 export let CurrentUser =  null;
 
-export async function Login(email) {
-    console.log(email);
-    const user = await myFetch('/users/login/'+email, { email }) ;
-    console.log("user data is");
-    console.log(user);
-    return user;
+export async function Login(username, password) {
+    const user = await myFetch('/users/login', { username, password }) ;
+
+    return CurrentUser = user;
 }
--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,76 +1,59 @@
 <template>
-  <form class="container form-down" @submit.prevent="login">
-      
-        <div class="columns hero is-primary">
-            <div class="column is-full title is-5 is-bold">
-               <p>Please Login with your credentials</p>
-             </div>
-      </div>  
-      <div class="field">
-        <p class="control has-icons-left has-icons-right">
-            <input class="input" type="email" placeholder="Email address" v-model="email">
-            <span class="icon is-small is-left">
-            <i class="fas fa-envelope"></i>
-            </span>
-            <span class="icon is-small is-right">
-            <i class="fas fa-check"></i>
-            </span>
-        </p>
-        </div>
-        <div class="field">
-        <p class="control has-icons-left">
-            <input class="input" type="password" placeholder="Password" >
-            <span class="icon is-small is-left">
-            <i class="fas fa-lock"></i>
-            </span>
-        </p>
-        </div>
-        <div class="field">
-        <p class="control">
-            <button class="button is-success is-medium" >
-            Login
-            </button>
-        </p>
-        </div>
-  </form>
+<form class ="container" @submit.prevent="login">
+  <div class="field">
+  <p class="control has-icons-left has-icons-right">
+    <input class="input" type="email" placeholder="Email" v-model="email">
+    <span class="icon is-small is-left">
+      <i class="fas fa-envelope"></i>
+    </span>
+    <span class="icon is-small is-right">
+      <i class="fas fa-check"></i>
+    </span>
+  </p>
+</div>
+<div class="field">
+  <p class="control has-icons-left">
+    <input class="input" type="password" placeholder="Password" v-model="password">
+    <span class="icon is-small is-left">
+      <i class="fas fa-lock"></i>
+    </span>
+  </p>
+</div>
+<div class="field">
+  <p class="control">
+    <button class="button is-success">
+      Log in
+    </button>
+  </p>
+</div>
+</form>
 </template>
-<script>
-import { Login } from "../models/Users";
 
-import {onClick} from "../views/Exercise"
+<script>
+import {Login } from "../models/Exercise1";
 export default {
-    data(){
-        return {
-            email:'',
-            error: ''
-        }
-    },
-    methods: {
-        async login(){
-            try {
-                console.log(this.email);
-                const user=await Login(this.email);
-                console.log("coimjhggfff");
-                console.log(user);
-                if(user!=null){
-                   this.$store.state.user=this.email;
-                   this.$router.push('/Exercise');
-                   document.getElementsByTagName("li")[0].className="is-active";
-                }else{
-                    alert("Sorry,You don't have the account to login,Please Sign up");
-                    this.$router.push('/SignUp');
-                }
-                console.log("outside is"+document.getElementsByTagName("a"));
-            } catch (error) {
-                this.error = error;
-            }
-            console.log("outside is"+document);
-        }
+  data(){
+      return {
+        email: '',
+        password: '',
+        error: ''
+      }
+  },
+  methods: {
+    login(){
+      try{
+          Login(this.email, this.password);
+          //if no error display the log
+          this.$router.push('Home');
+      } catch (error){
+          this.error = error;
+      }
     }
+  }
 }
 </script>
-<style scoped>
-  .form-down{
-      margin-top: 20px;
-  }
+
+<style>
 </style>
+
+  
