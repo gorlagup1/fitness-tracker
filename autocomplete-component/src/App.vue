e7414d6b40aa61b6500735bf9ee486def58c7dee
pavani <gorlagup1@hawkmail.newpaltz.edu>
2020-05-14 14:28:51 -0400

    final exam commit

--- autocomplete-component/src/App.vue
+++ autocomplete-component/src/App.vue
@@ -2,7 +2,9 @@
   <div id="app">
     <div style="margin:10% 25%;">
   <br>
-  <ejs-autocomplete>
+<ejs-autocomplete :dataSource='dataItem' :fields='dataFields'
+  placeholder="Select a exercise" popupWidth="250px"
+  popupHeight="400px">
   </ejs-autocomplete>
     </div>
   </div>
@@ -12,10 +14,17 @@
 import Vue from "vue";
 import { AutoCompletePlugin } from "@syncfusion/ej2-vue-dropdowns";
 Vue.use(AutoCompletePlugin);
+import { DataManager, WebApiAdaptor } from '@syncfusion/ej2-data';
 export default Vue.extend({
   data: function() {
 return {
-
+dataItem: new DataManager({
+  url: 'https://ej2services.syncfusion.com/production/web-services/api/Employees',
+  adaptor: new webApiAdaptor,
+  crossDomain: true
+}),
+ 
+dataFields:{value: 'Exercise'}
 };
 
   }
