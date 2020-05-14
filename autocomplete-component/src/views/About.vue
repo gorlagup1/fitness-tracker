d893487126b26101cb55181d34f26d432ceacaad
pavani <gorlagup1@hawkmail.newpaltz.edu>
2020-05-14 16:22:36 -0400

    final

--- autocomplete-component/src/views/About.vue
+++ autocomplete-component/src/views/About.vue
@@ -1,5 +1,36 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about flex-col item">
+    <input type="text" class="bg-gray-300 px-4 py-2" autocomplete="off" v-model="state">
+  <div v-if="filteredexercises && model">
+    <ul class="w-48 bg-gray-800 text-white">
+      <li v-for="filterexercises in filterexercises" in class="py-2 border-b cursor-pointer" @client>{{ filteredexercises}}</li>
+    </ul>
   </div>
 </template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      exercises: '',
+      model: false,
+      exercises: [
+        'Running',' Walking','Cycling','pushups','jogging'
+      ],
+      filterExercises: [],
+    }
+  },
+  
+
+methods: {
+  filterExercises(){
+    this.filterExercises = this.exercises.filter(exercises => {
+      return exercises.toLowercase().startsWith(this.exercises.toLowerCase());
+    });
+  },
+  setExercises(exercise){
+    this.exercise=exercise;
+  }
+}
+}
+</script>
