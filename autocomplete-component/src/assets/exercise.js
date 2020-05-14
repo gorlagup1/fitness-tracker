26c86080e4b7c109dc9c5d1c18ffbc44621411e8
pavani <gorlagup1@hawkmail.newpaltz.edu>
2020-05-14 15:41:33 -0400

    --- Auto-Git Commit ---

---
+++ autocomplete-component/src/assets/exercise.js
@@ -0,0 +1,22 @@
+export default [
+{id: 1, Exercisename: 'Jogging'},
+{id: 2, Exercisename: 'Walking'},
+{id: 3, Exercisename: 'Skiping'},
+{id: 4, Exercisename: 'Running'},
+{id: 5, Exercisename: 'Pushups'},
+{id: 6, Exercisename: 'Abdominals'},
+{id: 7, Exercisename: 'Lower back'},
+{id: 8, Exercisename: 'Quadriceps'},
+{id: 9, Exercisename: 'Leg press'},
+{id: 10, Exercisename: 'Leg extension'},
+{id: 11, Exercisename: 'Wall sit'},
+{id: 12,Exercisename: 'Hamstrings'},
+{id: 13, Exercisename: 'Calves'},
+{id: 14, Exercisename: 'Pectorals'},
+{id: 15, Exercisename: 'Lats'},
+{id: 16, Exercisename: 'Deltoids'},
+{id: 17, Exercisename: 'Triceps'},
+{id: 18, Exercisename: 'Biceps'},
+{id: 19, Exercisename: 'Weigh lifting'},
+{id: 20, Exercisename: 'Cycling'}
+];
---
+++ autocomplete-component/src/components/Autocomplete.vue
@@ -0,0 +1,39 @@
+<template>
+<div class="autocomplete">
+    <div class="input" @click="tooglevisible"></div>
+    </div>
+    </template>
+
+    <script>
+        export default {
+            data(){
+                return {
+                    visible: false
+                };
+
+                },
+                methods: {
+                    toogleVisible(){
+                        this.visible = !this.visible;
+                    }
+                }
+        }
+        </script>
+
+        <style scoped>
+            .autocomplete {
+                width: 100%;
+                position:relative;
+                }
+                .input {
+                    height: 40px;
+                    border-radius: 3px;
+                    border: 2px solid lightgray;
+                    box-shadow: 0 0 10px #eceaea;
+                    font-size:25px;
+                    padding-left:10px;
+                    padding-top:10px;
+                    cursor:text;
+                }
+        
+        
