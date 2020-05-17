31e9d8dacb9a114f7d3487c5a3082c0901b3d83c
pavani <gorlagup1@hawkmail.newpaltz.edu>
2020-05-17 11:42:14 -0400

    commited'

--- server/controllers/pics.js
+++ server/controllers/pics.js
@@ -2,5 +2,5 @@ const express = require('express');
 const pics = require('../models/Pics');
 const router = express.Router();
 router.get("/",(req,res)=> res.setHeader({
-    Pics:pics.Pics}))
+    P:pics.Pics}))
     module.exports = router;
