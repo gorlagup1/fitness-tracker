7dd0206c87e9e0520a4f63313c4b3d2ec6fcbcc9
pavani <gorlagup1@hawkmail.newpaltz.edu>
2020-05-17 11:44:45 -0400

    commit

--- server/controllers/pics.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const pics = require('../models/Pics');
-const router = express.Router();
-router.get("/",(req,res)=> res.setHeader({
-    P:pics.Pics}))
-    module.exports = router;
